890d123a58d2069e31d35e52f59e31e7f3cb7f08
Ruslan Golovizin <ruslan.golovizin@lamoda.ru>
2024-08-28 18:23:05 +0300

    Первые эксперименты с tui

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/rivo/tview"
+)
+
+func main() {
+	app := tview.NewApplication()
+	form := tview.NewForm().
+		AddDropDown("Title", []string{"Mr.", "Ms.", "Mrs.", "Dr.", "Prof."}, 0, nil).
+		AddInputField("First name", "", 20, nil, nil).
+		AddInputField("Last name", "", 20, nil, nil).
+		AddTextArea("Address", "", 40, 0, 0, nil).
+		AddTextView("Notes", "This is just a demo.\nYou can enter whatever you wish.", 40, 2, true, false).
+		AddCheckbox("Age 18+", false, nil).
+		AddPasswordField("Password", "", 10, '*', nil).
+		AddButton("Save", test).
+		AddButton("Quit", func() {
+			app.Stop()
+		})
+	form.SetBorder(true).SetTitle("Enter some data").SetTitleAlign(tview.AlignLeft)
+	if err := app.SetRoot(form, true).EnableMouse(true).EnablePaste(true).Run(); err != nil {
+		panic(err)
+	}
+}
+
+func test() {
+	fmt.Println("Hello world")
 }
