ea835d9effaeb1d4d4b2792bf6bf53e853a26cc7
Ruslan Golovizin <dcwk@yandex.ru>
2024-09-26 22:41:58 +0300

    Добавил сохранение authPair

--- internal/repository/auth_pair/repository.go
+++ internal/repository/auth_pair/repository.go
@@ -2,6 +2,9 @@ package auth_pair
 
 import (
 	"context"
+	"fmt"
+
+	sq "github.com/Masterminds/squirrel"
 
 	"github.com/dcwk/gophkeeper/internal/entity"
 	"github.com/dcwk/gophkeeper/internal/infra/db"
@@ -28,29 +31,28 @@ func NewRepository(db db.Client) repository.AuthPairRepository {
 }
 
 func (r *repo) CreateAuthPair(ctx context.Context, authPair *entity.AuthPair) (int64, error) {
-	//builder := sq.Insert(tableName).
-	//	PlaceholderFormat(sq.Dollar).
-	//	Columns(userIDColumn, typeColumn, nameColumn, createdAtColumn, updatedAtColumn).
-	//	Values("1", secret.SecretType, secret.Name, sq.Expr("NOW()"), sq.Expr("NOW()")).
-	//	Suffix("RETURNING id")
-	//
-	//query, args, err := builder.ToSql()
-	//if err != nil {
-	//	return 0, err
-	//}
-	//
-	//q := db.Query{
-	//	Name:     "secret_repository.CreateSecret",
-	//	QueryRaw: query,
-	//}
-	//
-	//var id int64
-	//err = r.db.DB().QueryRowContext(ctx, q, args...).Scan(&id)
-	//if err != nil {
-	//	fmt.Println(err.Error())
-	//	return 0, err
-	//}
-	//
-	//return id, nil
-	return 0, nil
+	builder := sq.Insert(tableName).
+		PlaceholderFormat(sq.Dollar).
+		Columns(secretIDColumn, loginColumn, passwordColumn, createdAtColumn, updatedAtColumn).
+		Values(authPair.Secret.ID, authPair.Login, authPair.Password, sq.Expr("NOW()"), sq.Expr("NOW()")).
+		Suffix("RETURNING id")
+
+	query, args, err := builder.ToSql()
+	if err != nil {
+		return 0, err
+	}
+
+	q := db.Query{
+		Name:     "auth_pair_repository.CreateAuthPair",
+		QueryRaw: query,
+	}
+
+	var id int64
+	err = r.db.DB().QueryRowContext(ctx, q, args...).Scan(&id)
+	if err != nil {
+		fmt.Println(err.Error())
+		return 0, err
+	}
+
+	return id, nil
 }
--- internal/service/secret/create_auth_pair.go
+++ internal/service/secret/create_auth_pair.go
@@ -7,14 +7,14 @@ import (
 )
 
 func (s *Service) CreateAuthPair(ctx context.Context, authPair *entity.AuthPair) (int64, error) {
-	var id int64
-
 	err := s.txManager.ReadCommitted(ctx, func(ctx context.Context) error {
 		var errTx error
-		id, errTx = s.secretRepository.CreateSecret(ctx, authPair.Secret)
+		authPair.Secret.ID, errTx = s.secretRepository.CreateSecret(ctx, authPair.Secret)
 		if errTx != nil {
 			return errTx
 		}
+
+		_, errTx = s.authPairRepository.CreateAuthPair(ctx, authPair)
 
 		return nil
 	})
@@ -23,5 +23,5 @@ func (s *Service) CreateAuthPair(ctx context.Context, authPair *entity.AuthPair)
 		return 0, err
 	}
 
-	return id, nil
+	return authPair.Secret.ID, nil
 }
